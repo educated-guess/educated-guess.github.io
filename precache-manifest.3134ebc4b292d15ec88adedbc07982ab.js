c8a2dec8624703cd679b518b4da4d1a9aa8dd103
Dave Niewinski <david_0429@hotmail.com>
2020-08-11 11:20:27 -0400

    Updates

---
+++ precache-manifest.3134ebc4b292d15ec88adedbc07982ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7a06469b20d3e1b61042a5e82a992a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fe2de317b0d799cd17e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "eab785a116530d467b37",
+    "url": "/static/js/2.74142ef2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.74142ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fe2de317b0d799cd17e",
+    "url": "/static/js/main.460ba60d.chunk.js"
+  },
+  {
+    "revision": "bedb49e9aebb65e0aae9",
+    "url": "/static/js/runtime-main.42db1411.js"
+  }
+]);
